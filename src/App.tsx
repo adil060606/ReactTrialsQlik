5fd6c6c9cd755e40bcfc969ea6d0f15bd71d14ca
Adi Lipin <adi.lipin@qlik.com>
2020-03-05 09:34:49 +0200

    q

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Button>ljji</Button>
+      <Button>ppppp</Button>
     </div>
   );
 }
